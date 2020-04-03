559f0a378f550999ae27cc932d7cd1fe8da5fa7f
mr-rampage <readonly@che.wbac.ca>
2020-04-03 09:55:15 -0400

    :art: fixing accessibility issues

--- src/component/details/details.js
+++ src/component/details/details.js
@@ -6,13 +6,13 @@ export function Details({text= 'Details', children}) {
 
   return (
     <>
-      <button className="uk-button uk-button-default" role="button" title="View country details" onClick={() => UIkit.modal(modalRef.current).show()}>
+      <button className="uk-button uk-button-default" title="View country details" onClick={() => UIkit.modal(modalRef.current).show()}>
         {text}
       </button>
       <div ref={modalRef} role="dialog" className="uk-modal-container uk-flex-top" data-uk-modal={true}>
         <div className='uk-modal-dialog uk-margin-auto-vertical uk-padding-small'>
           {children}
-          <button className="uk-modal-close-full uk-close-large" role="button" title="Close view country details" data-uk-close={true}/>
+          <button className="uk-modal-close-full uk-close-large" title="Close view country details" data-uk-close={true}/>
         </div>
       </div>
     </>
