d28ebdd9db50a1d54d5611ba617abb7ada784ae4
mr-rampage <readonly@che.wbac.ca>
2020-04-03 08:01:35 -0400

    Updates

---
+++ precache-manifest.ee96cecc020384c481b27308de6acf24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa08649b2dfb9a0ace93d75e3027b2f8",
+    "url": "/react-redux-with-hooks/index.html"
+  },
+  {
+    "revision": "7108be40d1f4006794bc",
+    "url": "/react-redux-with-hooks/static/css/main.fd319222.chunk.css"
+  },
+  {
+    "revision": "46f79fbe4716ee38e6f9",
+    "url": "/react-redux-with-hooks/static/js/2.4b84028e.chunk.js"
+  },
+  {
+    "revision": "a62744f3316a54e205d8437e7416374c",
+    "url": "/react-redux-with-hooks/static/js/2.4b84028e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7108be40d1f4006794bc",
+    "url": "/react-redux-with-hooks/static/js/main.d56e5731.chunk.js"
+  },
+  {
+    "revision": "72a9d1c1427fe43e9b8f",
+    "url": "/react-redux-with-hooks/static/js/runtime-main.04099ec8.js"
+  },
+  {
+    "revision": "6f8c208a659897d8492fee7e5e0a38d7",
+    "url": "/react-redux-with-hooks/static/media/world.6f8c208a.svg"
+  }
+]);
