b951d0ef2c64399fa9be7fc176ddbe1814f097f9
mr-rampage <readonly@che.wbac.ca>
2020-04-03 20:57:03 -0400

    :white_check_mark: added saga tests

--- src/domain/country/country.store.spec.js
+++ src/domain/country/country.store.spec.js
@@ -1,12 +1,44 @@
-import CountryReducer from './country.store';
+import CountryReducer, { countrySaga, FindCountryByCode } from './country.store';
+import { expectSaga } from 'redux-saga-test-plan';
+
+describe('Country Saga', () => {
+  it('should save country summary and details', () => {
+    const countryService = {
+      findByCountryCode: () => Promise.resolve({name: 'Canada'}),
+      findDetailsByCountryName: () => Promise.resolve('The true north strong and free!')
+    };
+
+    return expectSaga(countrySaga, countryService)
+      .withReducer(CountryReducer)
+      .dispatch(FindCountryByCode('CA'))
+      .hasFinalState({
+         name: 'Canada',
+         details: 'The true north strong and free!'
+      })
+      .run(10);
+  });
+
+  it('should not save country summary on error', () => {
+    const countryService = {
+      findByCountryCode: () => Promise.reject(new Error("Can't find country!")),
+      findDetailsByCountryName: () => Promise.resolve('The true north strong and free!')
+    };
+
+    return expectSaga(countrySaga, countryService)
+      .withReducer(CountryReducer)
+      .dispatch(FindCountryByCode('CA'))
+      .hasFinalState({})
+      .run(10);
+  });
+});
 
 describe('Country Reducer', () => {
   it('should append the country info on country fetched', () => {
     const actual = CountryReducer({name: 'test', details: 'world'}, {
       type: '[country] [fetch] [success]',
-      payload:  { name: 'hello' }
+      payload: {name: 'hello'}
     });
-    expect(actual).toEqual({ name: 'hello', details: 'world' });
+    expect(actual).toEqual({name: 'hello', details: 'world'});
   });
 
   it('should append the country details on country details fetched', () => {
@@ -14,7 +46,7 @@ describe('Country Reducer', () => {
       type: '[country details] [fetch] [success]',
       payload: 'foo'
     });
-    expect(actual).toEqual({ name: 'test', details: 'foo' });
+    expect(actual).toEqual({name: 'test', details: 'foo'});
   });
 
   it('should do nothing on all other actions', () => {
@@ -22,6 +54,6 @@ describe('Country Reducer', () => {
       type: 'unknown action',
       payload: 'foo'
     });
-    expect(actual).toEqual({ name: 'test', details: 'world' });
+    expect(actual).toEqual({name: 'test', details: 'world'});
   });
 });
